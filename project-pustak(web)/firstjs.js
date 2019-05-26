82cb3c523d6b03594c06ac4848b5aa578d6e973a
bipana50 <38601015+bipana50@users.noreply.github.com>
2019-05-26 18:15:35 +0545

    Delete firstjs.js

--- project-pustak(web)/firstjs.js
+++
@@ -1,7 +0,0 @@
-var firstName;
-firstName= prompt("give your first name");
-var lastname;
-lastname= prompt("give your last name");
-alert("your name is "+ firstName +" " + lastname) ;
-console.log("your name is "+ firstName +" " + lastname);
-console.log(57/4);
